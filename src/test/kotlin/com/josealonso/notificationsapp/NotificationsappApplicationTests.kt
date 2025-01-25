42fd724e844abff89373fb0f62409a44b0303863
josealonso <jose.alonso.programmer@gmail.com>
2025-01-25 02:16:17 +0100

    Create skeleton from the Spring Initializer. Use Kotlin and Gradle writtten in Kotlin.

---
+++ src/test/kotlin/com/josealonso/notificationsapp/NotificationsappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.josealonso.notificationsapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationsappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
