3962b0b3bf56d842ae5082afeb3b8fe306c3c327
josealonso <jose.alonso.programmer@gmail.com>
2025-01-29 16:10:21 +0100

    Write code for OrderService to publish notification events.

--- OrderService/src/main/kotlin/com/josealonso/com/josealonso/exceptions/UserNotFoundException.kt
+++
@@ -1,5 +0,0 @@
-package com.josealonso.com.josealonso.com.josealonso.exceptions
-
-class UserNotFoundException(userId: Long) : Throwable() {
-
-}
---
+++ OrderService/src/main/kotlin/com/josealonso/publisher/OrderNotificationPublisher.kt
@@ -0,0 +1,25 @@
+package com.josealonso.com.josealonso.publisher
+
+import com.josealonso.com.josealonso.entity.OrderStatus
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class OrderNotificationPublisher(private val kafkaTemplate: KafkaTemplate<String, OrderNotification>) {
+
+    fun publishOrderNotification(orderId: Long, status: OrderStatus, notificationType: NotificationType) {
+        val notification = OrderNotification(orderId, status, notificationType)
+        kafkaTemplate.send("order-notifications", orderId.toString(), notification)
+    }
+}
+
+// Class shared between microservices
+data class OrderNotification(
+    val orderId: Long,
+    val status: OrderStatus,
+    val notificationType: NotificationType
+)
+
+enum class NotificationType {
+    SMS, EMAIL
+}
